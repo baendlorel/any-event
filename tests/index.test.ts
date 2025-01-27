b2c07e01362cf6bf40f0613d519dd5e82cb7d11b
Aldia <futami16237@gmail.com>
2025-01-27 22:33:50 +0800

    Test: 加入两种简单情况的测试

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,44 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, jest, test } from '@jest/globals';
 import { EventBus } from '../src';
+const color = {
+  green: (text: string) => `\x1B[32m${text}\x1B[0m`,
+  yellow: (text: string) => `\x1B[33m${text}\x1B[0m`,
+  red: (text: string) => `\x1B[31m${text}\x1B[0m`,
+};
+
+const hr = () => {
+  console.log(color.green('==============='));
+};
 
 const bus = new EventBus();
 
 describe('EventBus class', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+  test('注册事件evt1并触发', () => {
+    return expect(
+      new Promise((resolve) => {
+        bus.on('evt1', () => {
+          resolve(true);
+        });
+        bus.logEventMaps();
+        bus.emit('evt1');
+      })
+    ).resolves.toBe(true);
+  });
+
+  test('注册事件evt2，上限2次，并触发3次', () => {
+    return expect(
+      new Promise((resolve) => {
+        const callback = jest.fn();
+        bus.on('evt2', callback, 2);
+        bus.logEventMaps();
+        bus.emit('evt2');
+        bus.emit('evt2');
+        bus.emit('evt2');
+        bus.logEventMaps();
+        setTimeout(() => {
+          resolve(callback);
+        }, 1000);
+      })
+    ).resolves.toBeCalledTimes(2);
   });
 });
